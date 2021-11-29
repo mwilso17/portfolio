a3e3d4f31244cea73954a55c1647056fe13a2141
mwilso17 <michael.b.wilson2@gmail.com>
2021-11-29 07:19:30 -0500

    additional changes

--- cafe/total_sales.py
+++ cafe/total_sales.py
@@ -1,5 +1,5 @@
 # Mike Wilson 29 November 2021
-# This program attempts to read the sales amounts in sales.txt and calculate
+# This program attempts to read the sales amounts in totals.txt and calculate
 # total sales.
 
 filename = 'cafe\\totals.txt'
@@ -9,13 +9,13 @@ total = 0
 
 h = open(filename, 'r')
 
-content = h.readlines()
+content = h.read()
 
 for line in content:
   for i in line:
-    if i.isdigit() == True:
-      print(i)
-      # total += int(i)
+    if i.isnumeric() == True:
+      # print(i)
+     total += int(i)
 
 print("Total sales so far: $", total)
 
