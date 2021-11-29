274c27a48c61df295bcd802f86dd75969048e1ab
mwilso17 <michael.b.wilson2@gmail.com>
2021-11-29 06:13:39 -0500

    added inventory and main program for cafe

---
+++ cafe.py
@@ -0,0 +1,39 @@
+# Mike Wilson 29 November 2021
+# This program attempts to emulate how a cafe's software may work to calculate
+# sales totals and record purchases. 
+
+from cafe_inventory import inventory, basket, subtotal
+
+# Tax rate can be adjusted by changing the value. 0.05 = 5% tax rate
+tax_rate = 0.05
+
+def calculate_total():
+  '''Accepts user input, calculates subtotal, adds tax, rounds total'''
+  active = True
+  while active:
+    user_input = input('Enter your next item (q to stop, i for inventory): ')
+
+    if user_input == 'q':
+      active = False
+    elif user_input == 'i':
+      print(inventory)
+      user_input
+    elif user_input not in inventory:
+      print('Please enter an item from the inventory list.')
+    elif user_input in inventory:
+      print(f'Adding your {user_input} to the cart.')
+      basket.append(user_input)
+  
+  print('Your cart contains: ', basket)
+
+  for items in basket:
+    subtotal.append(inventory[items])
+  pre_tax_total = sum(subtotal)
+
+  amount_to_pay = round(pre_tax_total + (pre_tax_total * tax_rate), 2)
+
+  print(f"Your total is: $", amount_to_pay)
+
+
+# Executes code
+calculate_total()
---
+++ cafe_inventory.py
@@ -0,0 +1,22 @@
+# Mike Wilson 29 November 2021
+# This file contains the inventory for our cafe. 
+# The inventory is stored in the format: "item name: price"
+
+inventory = {
+  'plain coffee': 2.09,
+  'latte': 3.19,
+  'mocha': 4.25,
+  'espresso': 2.19,
+  'soda': 2.59,
+  'cake slice': 4.19,
+  'pastry': 2.29,
+  'doughnut': 1.19,
+  'fresh fruit': 0.79,
+}
+
+
+
+
+# Additional lists to store customer purchases and subtotal
+subtotal = []
+basket = []
