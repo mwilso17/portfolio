91c0c0b6c9021f2013c1cc3df20abf670378e0a5
mwilso17 <michael.b.wilson2@gmail.com>
2021-12-09 07:31:20 -0500

    set background color

--- snowball_fight/snowball_fight.py
+++ snowball_fight/snowball_fight.py
@@ -12,6 +12,9 @@ class SnowballFight:
     self.screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Snowball Fight")
 
+    # Set background color
+    self.bg_color = (50, 230, 100)
+
   def run_game(self):
     '''Runs the main loop for the game'''
     while True:
@@ -19,6 +22,9 @@ class SnowballFight:
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
           sys.exit()
+
+      # Update screen during each loop
+      self.screen.fill(self.bg_color)
 
       # Update to most recent screen.
       pygame.display.flip()
