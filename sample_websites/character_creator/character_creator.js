db8a2b734c5599d05177412ebd6c0c567fc1f67f
mwilso17 <michael.b.wilson2@gmail.com>
2022-01-18 07:03:13 -0500

    test place holder function

--- sample_websites/character_creator/character_creator.js
+++ sample_websites/character_creator/character_creator.js
@@ -1,1 +1,14 @@
-alert("Welcome to character creator.")
+//Mike Wilson 18 Jan 2022: Attempt to make a character creator for tabletop RPGs.
+let maleFirstName = ['Estenian', 'Alph', 'Rollo'];
+let femaleFirstName = ['Freya', 'Eowyn', 'Lagertha'];
+let lastName = ['Greystorm', 'Miller', 'Smith'];
+
+var characterName = true;
+
+function validateName(){
+  alert('this is a function')
+}
+
+if (characterName == true) {
+  validateName();
+}
