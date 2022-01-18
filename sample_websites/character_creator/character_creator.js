b16671056036469b481f7175f770a283ace10795
mwilso17 <michael.b.wilson2@gmail.com>
2022-01-18 08:28:02 -0500

    started new function to handle stat generation

--- sample_websites/character_creator/character_creator.js
+++ sample_websites/character_creator/character_creator.js
@@ -1,12 +1,22 @@
 //Mike Wilson 18 Jan 2022: Attempt to make a character creator for tabletop RPGs.
-var characterName = true;
-
 var button = document.getElementById('buildMyCharacter');
 
 function validateCharacter(){
   let firstName = document.getElementById('firstName').value;
   let lastName = document.getElementById('lastName').value;
-  alert(`Your character's name is: ${firstName} ${lastName}`);
+  let characterName = firstName + ' ' + lastName;
+
+  let characterClass = document.getElementById('characterClass').value;
+
+  alert(`Your character's full name is: ${characterName} 
+  ${firstName} is a level 1 ${characterClass}`);
+}
+
+function generateCharacterStats(){
+  let race = document.getElementById('characterRace').value;
+  let characterClass = document.getElementById('characterClass').value;
+
+
 }
 
 button.addEventListener('click', validateCharacter);
