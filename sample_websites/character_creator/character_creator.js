473d3b9b40f6e3c49d0726bb0ad0e91b4a705947
mwilso17 <michael.b.wilson2@gmail.com>
2022-01-19 06:14:54 -0500

    added races, classes, and bonuses.

--- sample_websites/character_creator/character_creator.js
+++ sample_websites/character_creator/character_creator.js
@@ -1,22 +1,69 @@
 //Mike Wilson 18 Jan 2022: Attempt to make a character creator for tabletop RPGs.
 var button = document.getElementById('buildMyCharacter');
 
-function validateCharacter(){
+function generateCharacter(){
   let firstName = document.getElementById('firstName').value;
   let lastName = document.getElementById('lastName').value;
   let characterName = firstName + ' ' + lastName;
 
   let characterClass = document.getElementById('characterClass').value;
+  let race = document.getElementById('characterRace').value;
+
+  let strength = 1;
+  let wisdom = 1;
+  let intelligence = 1;
+  let dexterity = 1;
+  let charisma = 1;
+  let hitPoints = 1;
+  
+  if (race == 'Human') {
+    strength = strength + generateRandomNumber(20);
+    wisdom = wisdom + generateRandomNumber(20);
+    intelligence = intelligence + generateRandomNumber(20);
+    dexterity = dexterity + generateRandomNumber(20);
+    charisma = charisma + generateRandomNumber(20);
+    hitPoints = hitPoints + generateRandomNumber(20);
+  } else if (race == 'Elf') {
+    strength = strength + generateRandomNumber(14);
+    wisdom = wisdom + generateRandomNumber(25);
+    intelligence = intelligence + generateRandomNumber(26);
+    dexterity = dexterity + generateRandomNumber(23);
+    charisma = charisma + generateRandomNumber(15);
+    hitPoints = hitPoints + generateRandomNumber(14);
+  } else if (race == 'Dwarf') {
+    strength = strength + generateRandomNumber(23);
+    wisdom = wisdom + generateRandomNumber(17);
+    intelligence = intelligence + generateRandomNumber(14);
+    dexterity = dexterity + generateRandomNumber(17);
+    charisma = charisma + generateRandomNumber(12);
+    hitPoints = hitPoints + generateRandomNumber(26);
+  }
+
+  if (characterClass == 'Warrior') {
+    strength = strength + 4;
+    charisma = charisma + 3;
+  } else if (characterClass == 'Mage') {
+    wisdom = wisdom + 4;
+    intelligence = intelligence + 3;
+  } else if (characterClass == 'Archer') {
+    dexterity = dexterity + 4;
+    intelligence = intelligence + 3;
+  }
+
+  console.log(`Your attributes are: 
+    strength: ${strength}
+    wisdom: ${wisdom}
+    intelligence: ${intelligence}
+    dexterity: ${dexterity}
+    charisma: ${charisma}
+    hitPoints: ${hitPoints}`);
 
   alert(`Your character's full name is: ${characterName} 
-  ${firstName} is a level 1 ${characterClass}`);
+    ${firstName} is a level 1 ${characterClass}`);
 }
 
-function generateCharacterStats(){
-  let race = document.getElementById('characterRace').value;
-  let characterClass = document.getElementById('characterClass').value;
-//Add code to handle stat generation for 3 races. Human, elf, dwarf. Use if else if statements
-
+function generateRandomNumber(max) {
+  return Math.floor(Math.random() * max);
 }
 
-button.addEventListener('click', validateCharacter);
+button.addEventListener('click', generateCharacter);
